96d08efc8a62b3af375abacd569a7c5fd43edb1a
Andreas Siegel <mail@andreassiegel.de>
2017-05-21 10:30:37 +0200

    Turn application into Spring Boot app

--- src/main/java/cc/andreassiegel/helloecho/HelloEchoApplication.java
+++ src/main/java/cc/andreassiegel/helloecho/HelloEchoApplication.java
@@ -1,9 +1,12 @@
 package cc.andreassiegel.helloecho;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class HelloEchoApplication {
 
   public static void main(String[] args) {
-
+    SpringApplication.run(HelloEchoApplication.class, args);
   }
 }
---
+++ src/test/java/cc/andreassiegel/helloecho/HelloEchoApplicationTest.java
@@ -0,0 +1,16 @@
+package cc.andreassiegel.helloecho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloEchoApplicationTest {
+
+  @Test
+  public void contextLoads() {
+    // just test if the Spring application context loads successfully without throwing any exceptions
+  }
+}
